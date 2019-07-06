085b5930c93d815ce08c593ccde71c0cbd04f9b0
VytaliyYa <iweette@gmail.com>
2019-07-06 22:01:34 +0300

    Added solve 1.2-9

---
+++ 1st_week/1.2-9.py
@@ -0,0 +1,16 @@
+"""
+переменная objects только для локального запуска, в поле решения задачи на степике вставлять только решение.
+"""
+objects = [1, 2, 3, True, [12], 'as', 2, 3, 1]
+
+# solve:
+ans = 0
+
+s = set()
+for obj in objects:
+    i = id(obj)
+    if i not in s:
+        ans += 1
+        s.add(i)
+
+print(ans)
