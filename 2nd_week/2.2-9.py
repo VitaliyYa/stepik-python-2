9315e31e761b6aa932a42d000970c9f4d210764f
VytaliyYa <iweette@gmail.com>
2019-07-10 14:47:43 +0300

    Added solve 2.2-9, encrypted.bin ang passwords

--- 2nd_week/2.2-9.py
+++ 2nd_week/2.2-9.py
@@ -1,35 +1,13 @@
-# lib simple-crypt
-import urllib.request
-from simplecrypt import encrypt, decrypt, DecryptionException
-#
-# password = '123'
-# ciphertext = encrypt(password, 'my secret message')
-# print(ciphertext)
-# plaintext = decrypt(password, ciphertext)
-# print(plaintext)
+import simplecrypt
 
-url_file_encrypted = 'https://stepic.org/media/attachments/lesson/24466/encrypted.bin'
-url_file_passwords = 'https://stepic.org/media/attachments/lesson/24466/passwords.txt'
-file_encrypted = 'encrypted.bin'
-file_passwords = 'passwords.txt'
-
-encrypted_open = urllib.request.urlopen(url_file_encrypted).read()
-passwords_open = urllib.request.urlopen(url_file_passwords).read().strip().split()
-
-with open(file_encrypted, 'rb') as line:
-    encrypted = line.read()
-
-with open(file_passwords, 'rb') as p:
-    passwords = p.read().strip().split()
+encrypted = open('encrypted.bin', 'rb').read()
+passwords = open('passwords.txt').readlines()
 
 for p in passwords:
+    p = p.strip()
     try:
-        plain = decrypt(p, encrypted)
-    except DecryptionException:
-        pass
-    else:
-        plain = decrypt(p, encrypted)
-        print(plain)
+        s = simplecrypt.decrypt(p, encrypted)
+    except simplecrypt.DecryptionException:
+        continue
 
-# print(encrypted)
-print(passwords)
+    print(s.decode('utf-8'))
