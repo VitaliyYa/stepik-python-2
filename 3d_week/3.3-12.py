0e54e4545eaa42de18c2d5f6cfc6f978b5616575
VytaliyYa <iweette@gmail.com>
2019-07-15 13:32:49 +0300

    Added solve 3.3-12

---
+++ 3d_week/3.3-12.py
@@ -0,0 +1,21 @@
+"""
+Вам дана последовательность строк.
+В каждой строке замените все вхождения подстроки "human" на подстроку "computer"﻿ и выведите полученные строки.
+
+Sample Input:
+I need to understand the human mind
+humanity
+
+Sample Output:
+I need to understand the computer mind
+computerity
+"""
+
+
+import re
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    line = re.sub(r'human', 'computer', line)
+    print(line)
