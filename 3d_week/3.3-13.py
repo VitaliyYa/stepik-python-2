bf858e55febff9f4cf97f30fdb717a642348aec4
VytaliyYa <iweette@gmail.com>
2019-07-15 13:34:02 +0300

    Added solve 3.3-13

---
+++ 3d_week/3.3-13.py
@@ -0,0 +1,24 @@
+"""
+Вам дана последовательность строк.
+В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+
+Примечание:
+Обратите внимание на параметр count у функции sub.
+
+Sample Input:
+There’ll be no more "Aaaaaaaaaaaaaaa"
+AaAaAaA AaAaAaA
+
+Sample Output:
+There’ll be no more "argh"
+argh AaAaAaA
+"""
+
+
+import re
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    line = re.sub(r'\ba+\b', 'argh', line, count=1, flags=re.IGNORECASE)
+    print(line)
